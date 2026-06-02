00784aa7bdde03b22b59f33751d465f2ece89a4f
Boomathi <boomathi2047@gmail.com>
2026-06-02 12:33:55 +0530

    Implement isAnagram method in Solution class

---
+++ Java/242_ValidAnagram.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] s_str = s.toCharArray();
+        char[] t_str = t.toCharArray();
+        Arrays.sort(s_str);
+        Arrays.sort(t_str);
+        return Arrays.equals(s_str, t_str);
+    }
+}
