9ebfe777500acba6fbc9eacb3ea84b759e872821
Boomathi <boomathi2047@gmail.com>
2026-07-14 07:15:26 +0530

    Implement GCD calculation methods in GCD.java
    Added two methods for calculating GCD: one using a for loop and another using the Euclidean algorithm.

---
+++ Others/GCD.java
@@ -0,0 +1,40 @@
+// Using for loop
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n1 = scan.nextInt();
+        int n2 = scan.nextInt();
+        int gcd = 1;
+        
+        for (int i = 1; i <= n1 && i <= n2; i++){
+            if (n1 % i == 0 && n2 % i == 0){
+                gcd = i;
+            }
+        }
+        System.out.println("GCD: " + gcd);
+    }
+}
+
+// Using EuclideanAlgorithm
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n1 = scan.nextInt();
+        int n2 = scan.nextInt();
+
+        // Can be used for print statement
+        // int originaln1 = n1;
+        // int originaln2 = n2;
+        
+        while (n2 != 0){
+            int temp = n2;
+            n2 = n1 % n2;
+            n1 = temp;
+        }
+        System.out.println("GCD: "+n1);
+    }
+}
