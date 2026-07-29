2d139c980ecb401e0ba8729d3340366af117d9f6
Boomathi <boomathi2047@gmail.com>
2026-07-29 12:07:23 +0530

    Add brute-force solution for maximum subarray problem
    Implemented a brute-force solution to find the maximum subarray sum, which may exceed time limits for large inputs.

---
+++ Leetcode_Java/53_Maximum_Subarray.java
@@ -0,0 +1,20 @@
+// Optimal Solution
+
+
+// Time limit exceeded
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++){
+            int sum = 0;
+            for (int j = i; j < n; j++){
+                sum += nums[j];
+                if (sum > max){
+                    max = sum;
+                }
+            }
+        }
+        return max;
+    }
+}
