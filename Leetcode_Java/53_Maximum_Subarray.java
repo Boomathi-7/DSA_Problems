a661c950198e3ce1c15c25b26882e6648bcf8382
Boomathi <boomathi2047@gmail.com>
2026-07-29 13:24:40 +0530

    Add optimal solution for maximum subarray problem
    Implemented an optimal solution for finding the maximum subarray sum using Kadane's algorithm.

--- Leetcode_Java/53_Maximum_Subarray.java
+++ Leetcode_Java/53_Maximum_Subarray.java
@@ -1,5 +1,21 @@
 // Optimal Solution
-
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < n; i++){
+            sum += nums[i];
+            if (sum > max){
+                max = sum;
+            }
+            if (sum < 0){
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
 
 // Time limit exceeded
 class Solution {
