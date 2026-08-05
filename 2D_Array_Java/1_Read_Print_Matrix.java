e02a7efd3b60ba1c5caabb96b1507293753c2718
Boomathi <boomathi2047@gmail.com>
2026-08-05 09:08:26 +0530

    Implement matrix input and manual print functionality
    Added a new method to read and print matrix elements without using built-in functions.

--- 2D_Array_Java/1_Read_Print_Matrix.java
+++ 2D_Array_Java/1_Read_Print_Matrix.java
@@ -16,3 +16,27 @@ class Main{
         System.out.println(Arrays.deepToString(matrix));
     }
 }
+
+
+// Without using built-in function to print matrix elements
+
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+        scan.close();
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+        }
+    }
+}
