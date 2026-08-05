24f65472046ff67cecfc0660bb49ed4404f9e719
Boomathi <boomathi2047@gmail.com>
2026-08-05 09:09:29 +0530

    Implement row-wise printing of 2D array elements
    This program reads a 2D array from user input and prints its elements row-wise.

---
+++ 2D_Array_Java/2_Print_Elements_Rowwise.java
@@ -0,0 +1,23 @@
+// Print all elements row-wise
+
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+        scan.close();
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
