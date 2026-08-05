32453f97ac193796aade6b4068a6d1f2e575fbcf
Boomathi <boomathi2047@gmail.com>
2026-08-05 11:49:14 +0530

    Add program to find maximum element in 2D array
    Implement a Java program to find the maximum element in a 2D array.

---
+++ 2D_Array_Java/6_Find_Maximum_Element.java
@@ -0,0 +1,26 @@
+// Find the maximum element
+
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                matrix[i][m] = scan.nextInt();
+            }
+        }
+        scan.close();
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (matrix[i][j] > max){
+                    max = matrix[i][j];
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
