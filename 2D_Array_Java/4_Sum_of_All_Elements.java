7c7203a9b4b0a79177c795d470b14b559db01628
Boomathi <boomathi2047@gmail.com>
2026-08-05 10:17:16 +0530

    Add program to sum all elements in a 2D array
    Implement a Java program to calculate the sum of all elements in a 2D array.

---
+++ 2D_Array_Java/4_Sum_of_All_Elements.java
@@ -0,0 +1,24 @@
+// Find the sum of all elements
+
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+        scan.close();
+        int sum = 0;
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                sum += matrix[i][j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
