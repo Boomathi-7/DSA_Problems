d93009bff7812b111c4a40ce012ff8a3ab1ee9e4
Boomathi <boomathi2047@gmail.com>
2026-08-05 12:11:05 +0530

    Add minimum element finder for 2D array in Java
    Implement a Java program to find the minimum element in a 2D array.

---
+++ 2D_Array_Java/7_Find_Minimum_Element.java
@@ -0,0 +1,26 @@
+// Find the minimum element
+
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+        scan.close();
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (matrix[i][j] < min){
+                    min = matrix[i][j];
+                }
+            }
+        }
+        System.out.println(min);
+    }
+}
