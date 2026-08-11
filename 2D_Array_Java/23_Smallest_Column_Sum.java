cc36e5d8bec3aeeec4ad4497c5ba9bb497e0ebd7
Boomathi <boomathi2047@gmail.com>
2026-08-11 22:42:55 +0530

    Create 23_Smallest_Column_Sum.java

---
+++ 2D_Array_Java/23_Smallest_Column_Sum.java
@@ -0,0 +1,28 @@
+// Smallest column sum.
+
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+        scan.close();
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++){
+            int sum = 0;
+            for (int j = 0; j < m; j++){
+                sum += matrix[j][i];
+            }
+            if (sum < min){
+                min = sum;
+            }
+        }
+        System.out.println(min);
+    }
+}
