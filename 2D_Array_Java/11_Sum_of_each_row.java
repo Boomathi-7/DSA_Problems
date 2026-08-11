eb01b6499e97029a6236fff1486dc542af1394f8
Boomathi <boomathi2047@gmail.com>
2026-08-11 22:33:57 +0530

    Create 11_Sum_of_each_row.java

---
+++ 2D_Array_Java/11_Sum_of_each_row.java
@@ -0,0 +1,24 @@
+// Sum of each row.
+
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+        scan.close();
+        for (int i = 0; i < m; i++){
+            int sum = 0;
+            for (int j = 0; j < n; j++){
+                sum += matrix[i][j];
+            }
+            System.out.println(sum);
+        }
+    }
+}
