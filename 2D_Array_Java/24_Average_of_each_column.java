a78aad68a3b29064ca52d2d2af9f3ecb74a87257
Boomathi <boomathi2047@gmail.com>
2026-08-11 22:47:34 +0530

    Create 24_Average_of_each_column.java

---
+++ 2D_Array_Java/24_Average_of_each_column.java
@@ -0,0 +1,25 @@
+// Average of each column. 
+
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+        scan.close();
+        for (int i = 0; i < n; i++){
+            int sum = 0;
+            for (int j = 0; j < m; j++){
+                sum += matrix[j][i];
+            }
+            int average = sum/m;
+            System.out.println(average);
+        }
+    }
+}
