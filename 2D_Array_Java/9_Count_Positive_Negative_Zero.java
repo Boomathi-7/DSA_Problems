ac8aad9fe92a11d59f9a04b6ed0b2882b94fb56a
Boomathi <boomathi2047@gmail.com>
2026-08-11 21:57:04 +0530

    Create 9_Count_Positive_Negative_Zero.java

---
+++ 2D_Array_Java/9_Count_Positive_Negative_Zero.java
@@ -0,0 +1,36 @@
+// Count positive, negative and zero elements. 
+
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+        scan.close();
+        int positive = 0;
+        int negative = 0;
+        int zero = 0;
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (matrix[i][j] > 0){
+                    positive++;
+                }
+                else if (matrix[i][j] == 0){
+                    zero++;
+                }
+                else{
+                    negative++;
+                }
+            }
+        }
+        System.out.println("Positive: "+positive);
+        System.out.println("Negative: "+negative);
+        System.out.println("Zero: "+zero);
+    }
+}
