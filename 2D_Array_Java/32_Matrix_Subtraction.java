b08c19eaf4bebf8ec09d214d92460f22925d5db0
Boomathi <boomathi2047@gmail.com>
2026-08-12 22:05:05 +0530

    Create 32_Matrix_Subtraction.java

---
+++ 2D_Array_Java/32_Matrix_Subtraction.java
@@ -0,0 +1,30 @@
+// Matrix subtraction.
+
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] matrix1 = new int[m][n];
+        int[][] matrix2 = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                matrix1[i][j] = scan.nextInt();
+            }
+        }
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                matrix2[i][j] = scan.nextInt();
+            }
+        }
+        scan.close();
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                int difference = matrix1[i][j] - matrix2[i][j];
+                System.out.print(difference + " ");
+            }
+            System.out.println();
+        }
+    }
+}
