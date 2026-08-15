9f3b3870be50084ea5930b55420f34e72a6491aa
Boomathi <boomathi2047@gmail.com>
2026-08-15 21:54:12 +0530

    Add program to sum secondary diagonal of matrix
    This program calculates the sum of the secondary diagonal of a square matrix. It checks if the input matrix is square before performing the calculation.

---
+++ 2D_Array_Java/44_Sum_of_Secondary_Diagonal.java
@@ -0,0 +1,26 @@
+// Sum of secondary diagonal. 
+
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                matrix[i][j] = scan.nextInt();
+            }
+        }
+        scan.close();
+        if (m != n){
+            System.out.println("Not a Square Matrix.");
+            return;
+        }
+        int sum = 0;
+        for (int i = 0; i < m; i++){
+            sum += matrix[i][n-1-i];
+        }
+        System.out.println(sum);
+    }
+}
