c8a6a4ad3e61ed599379733ecde4ea0ad43b1d9d
Boomathi <boomathi2047@gmail.com>
2026-08-16 21:18:59 +0530

    Simplify transpose method by removing unnecessary loop

--- Leetcode_Java/867_Transpose_Matrix.java
+++ Leetcode_Java/867_Transpose_Matrix.java
@@ -1,10 +1,7 @@
 class Solution {
     public int[][] transpose(int[][] matrix) {
         int m = matrix.length;
-        int n = 0;
-        for (int i = 0; i < m; i++){
-            n = matrix[i].length;
-        }
+        int n = matrix[0].length;
         int[][] transpose = new int[n][m];
         for (int i = 0; i < m; i++){
             for (int j = 0; j < n; j++){
