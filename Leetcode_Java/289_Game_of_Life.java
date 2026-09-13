905a2cdd280f2b4133216e16009a10dc57a6a500
Boomathi <boomathi2047@gmail.com>
2026-09-13 19:36:28 +0530

    Add Game of Life implementation
    Implement the Game of Life algorithm to update the board based on the rules of the game.

---
+++ Leetcode_Java/289_Game_of_Life.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void gameOfLife(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                int liveNeighbors = 0;
+                for (int x = i-1; x <= i+1; x++){
+                    for (int y = j-1; y <= j+1; y++){
+                        if ((x >= 0 && x < m && y >= 0 && y < n) && (x != i || y != j) && (board[x][y] == 1 || board[x][y] == 2)){
+                            liveNeighbors += 1;
+                        }
+                    }
+                }
+                if (board[i][j] == 1 && (liveNeighbors < 2 || liveNeighbors > 3)) {
+                    board[i][j] = 2;
+                }
+                else if (board[i][j] == 0 && liveNeighbors == 3){
+                    board[i][j] = 3;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (board[i][j] == 2){
+                    board[i][j] = 0;
+                }
+                else if (board[i][j] == 3){
+                    board[i][j] = 1;
+                }
+            }
+        }
+    }
+}
