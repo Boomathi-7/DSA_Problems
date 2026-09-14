220fbf88cf235101ccdf78c69cba91db77bb6ddb
Boomathi <boomathi2047@gmail.com>
2026-09-14 19:19:03 +0530

    Add solution for counting number of enclaves
    Implement DFS to count enclaves in a grid.

---
+++ Leetcode_Java/1020_Number_of_Enclaves.java
@@ -0,0 +1,41 @@
+class Solution {
+    public void dfs(int[][] grid, int i, int j){
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0){
+            return;
+        }
+        grid[i][j] = 0;
+        dfs(grid, i-1, j);
+        dfs(grid, i+1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i, j+1);
+    }
+    public int numEnclaves(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+        for (int i = 0; i < m; i++){
+            if (grid[i][0] == 1){
+                dfs(grid, i, 0);
+            }
+            if (grid[i][n-1] == 1){
+                dfs(grid, i, n-1);
+            }
+        }
+        for (int j = 0; j < n; j++){
+            if (grid[0][j] == 1){
+                dfs(grid, 0, j);
+            }
+            if (grid[m-1][j] == 1){
+                dfs(grid, m-1, j);
+            }
+        }
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (grid[i][j] == 1){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
