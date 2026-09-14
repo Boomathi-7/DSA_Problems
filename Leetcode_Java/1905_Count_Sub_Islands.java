37d76618cb7b5350fbaca398697659959c118aaa
Boomathi <boomathi2047@gmail.com>
2026-09-14 19:00:46 +0530

    Implement optimal sub-island counting solution
    Added an optimal solution for counting sub-islands using DFS.

--- Leetcode_Java/1905_Count_Sub_Islands.java
+++ Leetcode_Java/1905_Count_Sub_Islands.java
@@ -33,3 +33,55 @@ class Solution {
         return count;
     }
 }
+
+
+// Optimal Solution
+class Solution {
+    private int[][] grid1;
+    private int[][] grid2;
+    private int m;
+    private int n;
+
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        this.grid1 = grid1;
+        this.grid2 = grid2;
+        m = grid1.length;
+        n = grid1[0].length;
+
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid2[i][j] == 1) {
+                    count += dfs(i, j);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private int dfs(int i, int j) {
+        int valid = grid1[i][j];
+
+        grid2[i][j] = 0;
+
+        if (i > 0 && grid2[i - 1][j] == 1) {
+            valid &= dfs(i - 1, j);
+        }
+
+        if (i + 1 < m && grid2[i + 1][j] == 1) {
+            valid &= dfs(i + 1, j);
+        }
+
+        if (j > 0 && grid2[i][j - 1] == 1) {
+            valid &= dfs(i, j - 1);
+        }
+
+        if (j + 1 < n && grid2[i][j + 1] == 1) {
+            valid &= dfs(i, j + 1);
+        }
+
+        return valid;
+    }
+}
